2161ac5bfc8c5369bd300125cead6115c487a876
sanskarb128-boop <sanskarb128@gmail.com>
2026-07-01 15:42:04 +0530

    Added protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ProtectedRoutes from "./components/ProtectedRoutes";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route
           path="/"
           element={
-          <ProtectedRoutes>
-            <Home />
-          </ProtectedRoutes>
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
           }
         />
 
         <Route path="/login" element={<Login />} />
-
         <Route path="/signup" element={<Signup />} />
+
+      
+        <Route path="*" element={<Navigate to="/login" />} />
+
       </Routes>
     </BrowserRouter>
   );
